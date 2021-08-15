cc49905b3b5dbb04e4061dc8f4cbc4ca216471f6
Riley <uyjustinriley@gmail.com>
2021-08-15 15:09:25 +0800

    no one asked

--- app/src/main/java/com/uy/esquivel/mobdeve_mp/GameActivity.java
+++ app/src/main/java/com/uy/esquivel/mobdeve_mp/GameActivity.java
@@ -1,4 +1,21 @@
 package com.uy.esquivel.mobdeve_mp;
 
-public class GameActivity {
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.uy.esquivel.mobdeve_mp.databinding.ActivityGameBinding;
+
+public class GameActivity extends AppCompatActivity {
+
+    private ActivityGameBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityGameBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
 }
--- app/src/main/java/com/uy/esquivel/mobdeve_mp/MainActivity.java
+++ app/src/main/java/com/uy/esquivel/mobdeve_mp/MainActivity.java
@@ -2,13 +2,28 @@ package com.uy.esquivel.mobdeve_mp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.uy.esquivel.mobdeve_mp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bStart = findViewById(R.id.b_start);
+
+        bStart.setOnClickListener(view -> {
+            Intent i = new Intent (MainActivity.this, GameActivity.class);
+            startActivity (i);
+        });
     }
+
+
 }
