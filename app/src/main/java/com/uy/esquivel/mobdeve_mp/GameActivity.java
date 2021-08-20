9a1e8e9b8625fd23a66a3488a6a78800c0350dfc
Riley <uyjustinriley@gmail.com>
2021-08-20 21:40:21 +0800

    Game proper completed, waiting for new assets

--- app/src/main/java/com/uy/esquivel/mobdeve_mp/GameActivity.java
+++ app/src/main/java/com/uy/esquivel/mobdeve_mp/GameActivity.java
@@ -8,7 +8,10 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,7 +28,7 @@ public class GameActivity extends AppCompatActivity {
 
     //game-related variables
     private String state;
-    private String asteroid_loc;
+    private int asteroid_loc;
     private int score;
 
     /*
@@ -33,7 +36,7 @@ public class GameActivity extends AppCompatActivity {
         player_state = 1; asteroid sequence plays, if player successfully dodges asteroid, player_state goes back to 0
         player_state = 2; player is dead
      */
-    private int player_state;
+    private int player_state = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +45,21 @@ public class GameActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        state = "lower_left";
-
         accelerometer = new Accelerometer(this);
         gyroscope = new Gyroscope(this);
+
+        state = "lower_left";
+        final ImageView img = findViewById(R.id.iv_ship);
+        img.setX(65);
+        img.setY(480);
+
+        /*
+        lower left x:65, y:480
+        lower right x:305, y:480
+        upper left x:65, y:180
+        upper right x:305, y:180
+         */
+
 
         Timer t = new Timer();
 
@@ -53,65 +67,89 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void run()
             {
+                TextView warning = findViewById(R.id.tv_warning);
+                switch (player_state){
+                    case 0:
+                        gyroscope.register();
+                        asteroid_loc = (int)Math.floor(Math.random()*(3-0+1)+0);
+                        Log.i ("PLAYER STATE", "ASTEROID POS: " + asteroid_loc);
 
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                switch (asteroid_loc){
+                                    case 0:
+                                        warning.setText("ASTEROID INCOMING:\nTOP LEFT");
+                                        break;
+                                    case 1:
+                                        warning.setText("ASTEROID INCOMING:\nTOP RIGHT");
+                                        break;
+                                    case 2:
+                                        warning.setText("ASTEROID INCOMING:\nBOTTOM LEFT");
+                                        break;
+                                    case 3:
+                                        warning.setText("ASTEROID INCOMING:\nBOTTOM RIGHT");
+                                        break;
+                                }
+                            }
+                        });
+                        player_state = 1;
+                        break;
+
+                    case 1:
+                        Log.i ("PLAYER STATE", state);
+                        Log.i ("UNREGISTERED", "gyro disabled");
+                        gyroscope.unregister();
+                        switch (asteroid_loc){
+                            case 0:
+                                if (state.equals("upper_left"))
+                                    player_state = 2;
+                                else
+                                    player_state = 0;
+                                break;
+                            case 1:
+                                if (state.equals("upper_right"))
+                                    player_state = 2;
+                                else
+                                    player_state = 0;
+                                break;
+                            case 2:
+                                if (state.equals("lower_left"))
+                                    player_state = 2;
+                                else
+                                    player_state = 0;
+                                break;
+                            case 3:
+                                if (state.equals("lower_right"))
+                                    player_state = 2;
+                                else
+                                    player_state = 0;
+                                break;
+                            default:
+                        }
+                        break;
+
+                    case 2:
+                        Log.i ("PLAYER STATE", "haha talo ./.");
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                warning.setText("HAHA TALO BOBO AHAAHAHAHAHAHAHAHAHAHAHAHAHAHAA");
+                            }
+                        });
+                        break;
+                    default:
+
+                }
             }
         };
 
-        t.schedule(tt,0,5000);
-
-//        accelerometer.setListener(new Accelerometer.Listener() {
-//            @Override
-//            public void onTranslation(float tX, float tY, float tZ) {
-//                ImageView img = (ImageView) findViewById(R.id.iv_bg);
-//                String states[] = new String[2];
-//                states = state.split ("_");
-//                if (tX > 1.0f){ //going right
-//
-//                    if (states[0] == "lower"){//if lower
-//                        if (states[1] == "left"){
-//                            state = "lower_right";
-//                            img.setImageResource(R.drawable.lowerright);
-//                        }
-//                        else{
-//                            state = "upper_right";
-//                            img.setImageResource(R.drawable.upperright);
-//                        }
-//
-//                    }
-//                    else { //if upper
-//                        if (states[1] == "left"){
-//                            state = "upper_right";
-//                            img.setImageResource(R.drawable.upperright);
-//                        }
-//                        else{
-//                            state = "upper_right";
-//                            img.setImageResource(R.drawable.upperright);
-//                        }
-//                    }
-//               }
-//               else if (tX < -1.0f){ //going left
-//                   if (states[0] == "lower"){ //if lower
-//                       if (states[1] == "right"){
-//                           state = "lower_left";
-//                           img.setImageResource(R.drawable.lowerleft);
-//                       }
-//
-//                   }
-//                   else{ //if upper
-//                       if (states[1] == "right"){
-//                           state = "upper_left";
-//                           img.setImageResource(R.drawable.upperleft);
-//                       }
-//                   }
-//
-//               }
-//            }
-//        });
+        t.schedule(tt,0,8000);
 
         gyroscope.setListener(new Gyroscope.Listener() {
             @Override
             public void onRotation(float rX, float rY, float rZ) {
-                ImageView img = findViewById(R.id.iv_bg);
                 String states[] = new String[2];
 
                 /*
@@ -123,7 +161,7 @@ public class GameActivity extends AppCompatActivity {
                  */
 
                 states = state.split ("_");
-                Log.i ("COORDS", "rX " + rX + " rY " + rY);
+                //Log.i ("COORDS", "rX " + rX + " rY " + rY);
                 if (Math.abs(rY)>Math.abs(rX)){
                     if (rY > 0.0f){ //rotate right
                         Log.i ("ROTATION", "Rotating right! " + states[0] + states[1]);
@@ -131,13 +169,15 @@ public class GameActivity extends AppCompatActivity {
                         if (states[0].equals("lower")){//if lower
                             if (states[1].equals("left")){
                                 state = "lower_right";
-                                img.setImageResource(R.drawable.lowerright);
+                                img.setX(305);
+                                img.setY(480);
                             }
                         }
                         else { //if upper
                             if (states[1].equals("left")){
                                 state = "upper_right";
-                                img.setImageResource(R.drawable.upperright);
+                                img.setX(305);
+                                img.setY(180);
                             }
                         }
                     }
@@ -148,29 +188,32 @@ public class GameActivity extends AppCompatActivity {
                         if (states[0].equals("lower")){//if lower
                             if (states[1].equals("right")){
                                 state = "lower_left";
-                                img.setImageResource(R.drawable.lowerleft);
+                                img.setX(65);
+                                img.setY(480);
                             }
                         }
                         else { //if upper
                             if (states[1].equals("right")){
                                 state = "upper_left";
-                                img.setImageResource(R.drawable.upperleft);
+                                img.setX(65);
+                                img.setY(180);
                             }
                         }
                     }
                 }
                 else{
                     if (rX > 0.0f){ //rotate downward
-
                         Log.i ("ROTATION", "Rotating downward! " + states[0] + states[1]);
                         if (states[0].equals("upper")){
                             if (states[1].equals("left")){
                                 state = "lower_left";
-                                img.setImageResource(R.drawable.lowerleft);
+                                img.setX(65);
+                                img.setY(480);
                             }
                             else{
                                 state = "lower_right";
-                                img.setImageResource(R.drawable.lowerright);
+                                img.setX(305);
+                                img.setY(480);
                             }
                         }
                     }
@@ -179,33 +222,31 @@ public class GameActivity extends AppCompatActivity {
                         if (states[0].equals("lower")){
                             if (states[1].equals("left")){
                                 state = "upper_left";
-                                img.setImageResource(R.drawable.upperleft);
+                                img.setX(65);
+                                img.setY(180);
                             }
                             else{
                                 state = "upper_right";
-                                img.setImageResource(R.drawable.upperright);
+                                img.setX(305);
+                                img.setY(180);
                             }
-
                         }
                     }
                 }
             }
         });
-
         play(view);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        accelerometer.register();
         gyroscope.register();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        accelerometer.unregister();
         gyroscope.unregister();
         stopPlayer();
     }
